6de1c705aa23a91e2d734930a658ded1c5970ea8
Angel Sosa <gabrielo.09.angel@gmail.com>
2025-12-27 22:44:50 -0500

    docs(project): add README and finalize project structure

---
+++ src/test/java/org/app/credit/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
