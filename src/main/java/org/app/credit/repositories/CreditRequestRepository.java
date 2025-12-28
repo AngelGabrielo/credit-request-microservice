d72b80fa3bff925cf2c15c6cca55a97234dd721d
Angel Sosa <gabrielo.09.angel@gmail.com>
2025-12-27 22:38:43 -0500

    feat(data): add JPA repositories for database access

---
+++ src/main/java/org/app/credit/repositories/CreditRequestRepository.java
@@ -0,0 +1,13 @@
+package org.app.credit.repositories;
+
+import org.app.credit.entities.CreditRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CreditRequestRepository extends JpaRepository<CreditRequest,Long> {
+    List<CreditRequest> findByUserUsername(String username);
+}
---
+++ src/main/java/org/app/credit/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package org.app.credit.repositories;
+
+import org.app.credit.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String name);
+}
